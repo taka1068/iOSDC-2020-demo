6eca0b32dc4955dd92888f9ec90007253e4a6bb2
TakanoriHirobe <london.2xxx@gmail.com>
2020-09-20 05:22:30 +0900

    add

--- demo/Sources/ConcurrencyWithGCD/concurrentmap.swift
+++ demo/Sources/ConcurrencyWithGCD/concurrentmap.swift
@@ -24,6 +24,18 @@ extension Array {
         return result.map { $0! }
         
     }
+    
+    func concurrentMap2<T>(_ transform: (Element) -> T) -> [T] {
+        // https://taka1068.hatenablog.jp/entry/2020/09/20/034600
+        var result = ContiguousArray<T?>.init(repeating: nil, count: count)
+        
+        result.withUnsafeMutableBufferPointer { buffer in
+            DispatchQueue.concurrentPerform(iterations: buffer.count) { index in
+                buffer[index] = transform(self[index])
+            }
+        }
+        return result.map { $0! }
+    }
 }
 
 func concurrentMapBenchmark() {
@@ -31,6 +43,7 @@ func concurrentMapBenchmark() {
     print(greetings.count)
     var charCount1: Int?
     var charCount2: Int?
+    var charCount3: Int?
     
     benchmark(title: "charCountWithMap") {
         charCount1 = greetings.map { $0.count }.reduce(0, +)
@@ -40,7 +53,11 @@ func concurrentMapBenchmark() {
         charCount2 = greetings.concurrentMap { $0.count }.reduce(0, +)
     }
     
-    print(charCount1! == charCount2!)
+    benchmark(title: "charCountWithConcurrentMap2") {
+        charCount3 = greetings.concurrentMap2 { $0.count }.reduce(0, +)
+    }
+    
+    print(charCount1! == charCount2!, charCount1! == charCount3!)
     
     //
     
@@ -50,6 +67,7 @@ func concurrentMapBenchmark() {
 
     var ideographicCount1: Int?
     var ideographicCount2: Int?
+    var ideographicCount3: Int?
     
     benchmark(title: "ideographicCountWithMap") {
         ideographicCount1 = largeStringArray.map(countIdeographicCharacters).reduce(0, +)
@@ -58,8 +76,10 @@ func concurrentMapBenchmark() {
     benchmark(title: "ideographicCountWithConcurrentMap") {
         ideographicCount2 = largeStringArray.concurrentMap(countIdeographicCharacters).reduce(0, +)
     }
-
-    print(ideographicCount1! == ideographicCount2!)
     
-    print("kokoro count", largeStringArray.map { $0.count }.reduce(0, +))
+    benchmark(title: "ideographicCountWithConcurrentMap2") {
+        ideographicCount3 = largeStringArray.concurrentMap2(countIdeographicCharacters).reduce(0, +)
+    }
+
+    print(ideographicCount1! == ideographicCount2!, ideographicCount1! == ideographicCount3!)
 }
